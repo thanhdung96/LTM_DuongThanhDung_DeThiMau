062d7e084e89a0bc3f877e2af0d258cd9a9ea818
thanhdung96 <14dh110224@st.huflit.edu.vn>
2017-12-04 11:56:02 +0700

    Finished server read db

---
+++ Domains/Item.cs
@@ -0,0 +1,34 @@
+﻿namespace Domains
+{
+	public class Item
+	{
+		private string type;
+		private int stock;
+		private int price;
+
+		public Item()
+		{
+			this.type = "";
+			this.stock = 0;
+			this.price = 0;
+		}
+
+		public string Type
+		{
+			get { return type; }
+			set { type = value; }
+		}
+
+		public int Stock
+		{
+			get { return stock; }
+			set { stock = value; }
+		}
+
+		public int Price
+		{
+			get { return price; }
+			set { price = value; }
+		}
+	}
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using Domains;
+using System.IO;
 
 namespace Server
 {
 	class Program
 	{
+		private static List<Item> database;
+
+		private static void readDB()
+		{
+			//preparing
+			XmlDataDocument xmlDoc = new XmlDataDocument();
+			XmlNodeList xmlNode;
+			database = new List<Item>();
+			//read 
+			FileStream fs = new FileStream(@".\database.xml", FileMode.Open, FileAccess.Read);
+			xmlDoc.Load(fs);
+			xmlNode = xmlDoc.GetElementsByTagName("Item");
+			foreach (XmlNode node in xmlNode)
+			{
+				Item item = new Item();
+				item.Type = node.ChildNodes.Item(0).InnerText.Trim();
+				item.Stock = XmlConvert.ToInt32(node.ChildNodes.Item(1).InnerText.Trim());
+				item.Price = XmlConvert.ToInt32(node.ChildNodes.Item(2).InnerText.Trim());
+			}
+		}
+
 		static void Main(string[] args)
 		{
+			readDB();
 		}
 	}
 }
