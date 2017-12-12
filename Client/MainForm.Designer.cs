fb30d0a118d81799828e03d03c73c24b7d4876f6
thanhdung96 <14dh110224@st.huflit.edu.vn>
2017-12-12 10:44:25 +0700

    Designed client gui

---
+++ Client/MainForm.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Client
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dgvListItems = new System.Windows.Forms.DataGridView();
+			this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Stock = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.dgvOrder = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtID = new System.Windows.Forms.TextBox();
+			this.txType = new System.Windows.Forms.TextBox();
+			this.txtQuantity = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.btnRefresh = new System.Windows.Forms.Button();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.btnRemove = new System.Windows.Forms.Button();
+			this.btnPay = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dgvListItems)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).BeginInit();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// dgvListItems
+			// 
+			this.dgvListItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvListItems.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Type,
+            this.Stock,
+            this.Price});
+			this.dgvListItems.Dock = System.Windows.Forms.DockStyle.Top;
+			this.dgvListItems.Location = new System.Drawing.Point(0, 0);
+			this.dgvListItems.Name = "dgvListItems";
+			this.dgvListItems.ReadOnly = true;
+			this.dgvListItems.Size = new System.Drawing.Size(527, 153);
+			this.dgvListItems.TabIndex = 0;
+			// 
+			// Id
+			// 
+			this.Id.HeaderText = "ID";
+			this.Id.Name = "Id";
+			this.Id.ReadOnly = true;
+			// 
+			// Type
+			// 
+			this.Type.HeaderText = "Type";
+			this.Type.Name = "Type";
+			this.Type.ReadOnly = true;
+			// 
+			// Stock
+			// 
+			this.Stock.HeaderText = "Stock";
+			this.Stock.Name = "Stock";
+			this.Stock.ReadOnly = true;
+			// 
+			// Price
+			// 
+			this.Price.HeaderText = "Price";
+			this.Price.Name = "Price";
+			this.Price.ReadOnly = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.groupBox1.Location = new System.Drawing.Point(0, 153);
+			this.groupBox1.Margin = new System.Windows.Forms.Padding(10);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(527, 339);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Your Order";
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 3;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+			this.tableLayoutPanel1.Controls.Add(this.txtQuantity, 2, 2);
+			this.tableLayoutPanel1.Controls.Add(this.txType, 2, 1);
+			this.tableLayoutPanel1.Controls.Add(this.dgvOrder, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label3, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.txtID, 2, 0);
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 3);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 16);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 4;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(521, 320);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// dgvOrder
+			// 
+			this.dgvOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvOrder.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dgvOrder.Location = new System.Drawing.Point(3, 3);
+			this.dgvOrder.Name = "dgvOrder";
+			this.tableLayoutPanel1.SetRowSpan(this.dgvOrder, 3);
+			this.dgvOrder.Size = new System.Drawing.Size(254, 282);
+			this.dgvOrder.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(263, 83);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(46, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "ID";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(263, 137);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(46, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Type";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(263, 192);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(46, 13);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Quantity";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// txtID
+			// 
+			this.txtID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtID.Location = new System.Drawing.Point(315, 73);
+			this.txtID.Name = "txtID";
+			this.txtID.Size = new System.Drawing.Size(203, 20);
+			this.txtID.TabIndex = 4;
+			// 
+			// txType
+			// 
+			this.txType.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.txType.Location = new System.Drawing.Point(315, 134);
+			this.txType.Name = "txType";
+			this.txType.Size = new System.Drawing.Size(203, 20);
+			this.txType.TabIndex = 5;
+			// 
+			// txtQuantity
+			// 
+			this.txtQuantity.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtQuantity.Location = new System.Drawing.Point(315, 195);
+			this.txtQuantity.Name = "txtQuantity";
+			this.txtQuantity.Size = new System.Drawing.Size(203, 20);
+			this.txtQuantity.TabIndex = 6;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 4;
+			this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 3);
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel2.Controls.Add(this.btnPay, 3, 0);
+			this.tableLayoutPanel2.Controls.Add(this.btnRemove, 2, 0);
+			this.tableLayoutPanel2.Controls.Add(this.btnAdd, 1, 0);
+			this.tableLayoutPanel2.Controls.Add(this.btnRefresh, 0, 0);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 288);
+			this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(521, 32);
+			this.tableLayoutPanel2.TabIndex = 7;
+			// 
+			// btnRefresh
+			// 
+			this.btnRefresh.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.btnRefresh.Location = new System.Drawing.Point(3, 3);
+			this.btnRefresh.Name = "btnRefresh";
+			this.btnRefresh.Size = new System.Drawing.Size(124, 26);
+			this.btnRefresh.TabIndex = 0;
+			this.btnRefresh.Text = "Refresh";
+			this.btnRefresh.UseVisualStyleBackColor = true;
+			this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.btnAdd.Location = new System.Drawing.Point(133, 3);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(124, 26);
+			this.btnAdd.TabIndex = 1;
+			this.btnAdd.Text = "Add";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+			// 
+			// btnRemove
+			// 
+			this.btnRemove.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.btnRemove.Location = new System.Drawing.Point(263, 3);
+			this.btnRemove.Name = "btnRemove";
+			this.btnRemove.Size = new System.Drawing.Size(124, 26);
+			this.btnRemove.TabIndex = 2;
+			this.btnRemove.Text = "Remove";
+			this.btnRemove.UseVisualStyleBackColor = true;
+			this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+			// 
+			// btnPay
+			// 
+			this.btnPay.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.btnPay.Location = new System.Drawing.Point(393, 3);
+			this.btnPay.Name = "btnPay";
+			this.btnPay.Size = new System.Drawing.Size(125, 26);
+			this.btnPay.TabIndex = 3;
+			this.btnPay.Text = "Pay";
+			this.btnPay.UseVisualStyleBackColor = true;
+			this.btnPay.Click += new System.EventHandler(this.btnPay_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(527, 492);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.dgvListItems);
+			this.Name = "MainForm";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dgvListItems)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).EndInit();
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dgvListItems;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Stock;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.DataGridView dgvOrder;
+		private System.Windows.Forms.TextBox txtQuantity;
+		private System.Windows.Forms.TextBox txType;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtID;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.Button btnPay;
+		private System.Windows.Forms.Button btnRemove;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnRefresh;
+	}
+}
+
---
+++ Client/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using Domains;
+
+namespace Client
+{
+	public partial class MainForm : Form
+	{
+		private const int BUFFER_SIZE = 1024;
+		private List<Item> order, listItems;
+		TcpClient client;
+		NetworkStream netstream;
+		byte[] dataSend, dataReceive;
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void btnRefresh_Click(object sender, EventArgs e)		//send LIST_REQUEST
+		{
+
+		}
+
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnRemove_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnPay_Click(object sender, EventArgs e)		//send ORDER_RTS then PAYMENT_REQUEST
+		{
+
+		}
+	}
+}
